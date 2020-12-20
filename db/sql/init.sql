76ef81bfc65615216732cf6517497b417cdb4ae3
llNoahll <537520539@qq.com>
2020-12-20 16:41:04 +0800

    Modify Bug in init.sql

--- db/sql/init.sql
+++ db/sql/init.sql
@@ -1,9 +1,9 @@
 INSERT INTO `Manager` (`mid`, `login_name`, `login_pwd`, `nickname`, `permission`)
 VALUES ('5fdea7e918cc6', '320180940752', '123456', 'M1', 1);
 
-INSERT INTO `Shop` (`sid`, `login_name`, `login_pwd`, `shop_name`, `boss_name`, `phone`, `email`, `address`, `isrecommend`)
-VALUES ('5fdea7f1202ba', '320180940750', '123456', '网和豆腐', '李华', '13646543128', 'test@lzu.edu.cn', '兰大', 0),
-       ('5fdea80f62db9', '320180940753', '123456', '网和牛肉', '小红', '13642043128', 'test@lzu.edu.cn', '兰大', 0);
+INSERT INTO `Shop` (`sid`, `login_name`, `login_pwd`, `shop_name`, `boss_name`, `foods_msg`, `phone`, `email`, `address`, `isrecommend`)
+VALUES ('5fdea7f1202ba', '320180940750', '123456', '网和豆腐', '李华', '{"5fdebd669ee1d":{"fid":"5fdebd669ee1d","fname":"\u9ebb\u5a46\u8c46\u8150","price":32,"brief":"\u597d\u5403\u7684\u9ebb\u5a46\u8c46\u8150","img_path":".\/food1.png"},"5fdebd763440a":{"fid":"5fdebd763440a","fname":"\u9752\u6912\u8c46\u5e72","price":22,"brief":"\u597d\u5403","img_path":".\/food2.png"},"5fdebd7bd1986":{"fid":"5fdebd7bd1986","fname":"\u5bab\u4fdd\u9e21\u4e01","price":22,"brief":"\u4e0d\u597d\u5403","img_path":".\/food3.png"}}', '13646543128', 'test@lzu.edu.cn', '兰大', 0),
+       ('5fdea80f62db9', '320180940753', '123456', '网和牛肉', '小红', '{"5fdebd669ee1d":{"fid":"5fdebd669ee1d","fname":"\u9ebb\u5a46\u8c46\u8150","price":32,"brief":"\u597d\u5403\u7684\u9ebb\u5a46\u8c46\u8150","img_path":".\/food1.png"},"5fdebd763440a":{"fid":"5fdebd763440a","fname":"\u9752\u6912\u8c46\u5e72","price":22,"brief":"\u597d\u5403","img_path":".\/food2.png"},"5fdebd7bd1986":{"fid":"5fdebd7bd1986","fname":"\u5bab\u4fdd\u9e21\u4e01","price":22,"brief":"\u4e0d\u597d\u5403","img_path":".\/food3.png"}}', '13642043128', 'test@lzu.edu.cn', '兰大', 0);
 
 INSERT INTO `Customer` (`cid`, `login_name`, `login_pwd`, `pay_pwd`, `sex`, `age`, `qq`, `phone`, `nickname`, `realname`, `email`, `address`)
 VALUES ('5fdea83ccd3d2', '320180940751', '123456', '666666', '男', 20, '354321384', '13656438496', 'C1', '李亮', 'test@lzu.edu.cn', '兰大'),
